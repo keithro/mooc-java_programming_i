733bbddad3faefd4324a532b7733d69220bc5815
keithro <rodriguez.keith@gmail.com>
2023-11-23 23:10:19 -0600

    Section 02-02

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,13 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to? ");
+        int end = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from? ");
+        int start = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }  
     }
 }
